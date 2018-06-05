aea59e004168c47150737c0536c173323fc1b95c
Avik Samaddar <eml2avik@gmail.com>
2018-06-05 15:58:04 +0530

    Update _version.py

--- happybase/_version.py
+++ happybase/_version.py
@@ -1,8 +1,8 @@
 """
 HappyBase version module.
-
+This version represents a forked version from 1.1.0 
 This module defines the package version for use in __init__.py and
 setup.py.
 """
 
-__version__ = '1.1.0'
+__version__ = '1.1.f'
